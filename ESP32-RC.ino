3b361b9e49eb63dd579f1ffefd29fe6e646b1e0a
Runa-chama <88036533+Runa-chama@users.noreply.github.com>
2022-01-31 20:11:24 +0900

    First

---
+++ ESP32-RC.ino
@@ -0,0 +1,216 @@
+#include <ESP32Servo.h>
+#include <Ps3Controller.h>
+
+
+#define ON LOW
+#define OFF HIGH
+
+#define battery_pin A10
+
+//motor
+#define m1 32
+#define m2 33
+#define m_ch1 10
+#define m_ch2 11
+
+//servo
+#define servo_pin 13
+
+//front lump
+#define led_front_1 16
+#define led_front_2 5
+
+//back lump
+#define led_back_1 25
+#define led_back_2 21
+
+//brake lump
+#define led_brake_1 27
+#define led_brake_2 23
+
+//turn signal lump (right)
+#define turn_lump_r1 14
+#define turn_lump_r2 26
+//turn signal lump (left)
+#define turn_lump_l1 15
+#define turn_lump_l2 22
+
+//led_pins
+const int leds[] = {led_front_1,led_front_2,led_back_1,led_back_2,led_brake_1,led_brake_2,turn_lump_r1,turn_lump_r2,turn_lump_l1,turn_lump_l2};
+
+const int battery_min_voltage = 2.10;
+
+int minUs = 500;
+int maxUs = 2400;
+Servo servo;
+
+volatile int interruptCounter;
+hw_timer_t * timer = NULL;
+
+bool is_remaining_charge_enough = true;
+bool is_turn_right = false;
+bool is_turn_left = false;
+
+int speed = 0;
+
+//Callback function When a button pushed
+void notify(){
+  int stick_left_y = Ps3.data.analog.stick.ly;
+  int stick_right_x = Ps3.data.analog.stick.rx;
+  
+  if(is_remaining_charge_enough)
+  {
+    int cache_speed = speed;
+    double max_speed = 140;
+    speed = stick_left_y >= 0 ? map(stick_left_y,10,128,0,max_speed) : map(-stick_left_y,10,128,0,max_speed);
+
+    if(stick_left_y < 65 && stick_left_y > -65)
+    {
+      digitalWrite(led_brake_1,ON);
+      digitalWrite(led_brake_2,ON);
+    }
+    else
+    {
+      digitalWrite(led_brake_1,OFF);
+      digitalWrite(led_brake_2,OFF);
+    }
+      
+      
+    if(stick_left_y < -10) //go
+    {
+       ledcWrite(m_ch1,speed);
+       ledcWrite(m_ch2,0);
+    }
+    else if(stick_left_y > 10) //back
+    {
+      ledcWrite(m_ch1,0);
+      ledcWrite(m_ch2,speed);
+      digitalWrite(led_back_1,ON);
+      digitalWrite(led_back_2,ON);
+    }
+    else
+    {
+      ledcWrite(m_ch1,0);
+      ledcWrite(m_ch2,0);
+      digitalWrite(led_back_1,OFF);
+      digitalWrite(led_back_2,OFF);
+    }
+
+    if(stick_right_x < -10) //right
+    {
+      int angle = 90 + map(-stick_right_x,10,180,0,60);
+      servo.write(angle);
+    }
+    else if(stick_right_x > 10) //left
+    {
+      int angle = 90 - map(stick_right_x,10,180,0,60);
+      servo.write(angle);
+    }
+    else
+    {
+      servo.write(90);
+    }
+    
+    if(Ps3.data.button.l1)
+    {
+      is_turn_right = false;
+      is_turn_left = true;
+    }
+    else if(Ps3.data.button.r1)
+    {
+      is_turn_right = true;
+      is_turn_left = false;
+    }
+    else
+    {
+      is_turn_right = false;
+      is_turn_left = false;
+    }
+    if(Ps3.data.button.circle)
+    {
+      digitalWrite(led_front_1,!digitalRead(led_front_1));
+      digitalWrite(led_front_2,!digitalRead(led_front_2));
+    }
+  }
+}
+
+void break_lump(int i){
+  digitalWrite(led_brake_1,i);
+  digitalWrite(led_brake_2,i);
+}
+
+void reset_turn_lump()
+{
+    digitalWrite(turn_lump_l1,OFF);
+    digitalWrite(turn_lump_l2,OFF);
+    digitalWrite(turn_lump_r1,OFF);
+    digitalWrite(turn_lump_r2,OFF);
+}
+
+
+void IRAM_ATTR onTimer() {
+//turn signal
+  if(is_turn_right)
+  {
+    digitalWrite(turn_lump_r1,!digitalRead(turn_lump_r1));
+    digitalWrite(turn_lump_r2,!digitalRead(turn_lump_r2));
+    digitalWrite(turn_lump_l1,OFF);
+    digitalWrite(turn_lump_l2,OFF);
+  }
+  else if(is_turn_left)
+  {
+    digitalWrite(turn_lump_l1,!digitalRead(turn_lump_l1));
+    digitalWrite(turn_lump_l2,!digitalRead(turn_lump_l2));
+    digitalWrite(turn_lump_r1,OFF);
+    digitalWrite(turn_lump_r2,OFF);
+  }
+  else
+  {
+    reset_turn_lump();
+  }
+}
+
+void setup(){
+  pinMode(12,OUTPUT);
+  digitalWrite(12,HIGH);
+  delay(100);
+  digitalWrite(12,LOW);
+  for(int led : leds)
+  {
+    pinMode(led,OUTPUT_OPEN_DRAIN);
+    digitalWrite(led,OFF);
+  }
+
+  pinMode(battery_pin,INPUT);
+  
+  //pwm setup
+  ledcSetup(m_ch1,450,8);
+  ledcSetup(m_ch2,450,8);
+  ledcAttachPin(m1,m_ch1);//motor ch1
+  ledcAttachPin(m2,m_ch2);//motor ch2
+  
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, &onTimer, true);
+  timerAlarmWrite(timer, 250000, true);
+  timerAlarmEnable(timer);
+  
+  servo.setPeriodHertz(50);
+  servo.attach(servo_pin,minUs,maxUs);
+
+  Serial.begin(115200);
+  Ps3.attach(notify); //Set callback function
+  Ps3.begin("00:00:00:00:00:00"); //Bluetooth MAC Address
+}
+
+void loop(){
+  //check voltage of batterys
+  float battery_voltage = analogRead(battery_pin);
+  battery_voltage /= 4096;
+  battery_voltage *= 3.3;
+  Serial.println(analogRead(battery_pin));
+  Serial.println(battery_voltage);
+  if(battery_voltage < battery_min_voltage) 
+    is_remaining_charge_enough = false;
+  else
+    is_remaining_charge_enough = true;
+}
---
+++ README.md
@@ -0,0 +1,38 @@
+# RemoteCar-ESP32
+
+
+This is a program of Remote Car which works on ESP32 development board.
+___
+
+### **[Used Librarys]**
+- [Ps3Controller for ESP32](https://github.com/jvpernis/esp32-ps3/)
+- Servo
+___
+
+### **Circuit**
+![Circuit]
+
+___
+### **How to connect PS3 controller**
+1.Check the MAC Address of ESP32.
+```c
+void setup(void) {
+  Serial.begin(115200);
+  uint8_t btmac[6];
+  esp_read_mac(btmac, ESP_MAC_BT);
+  Serial.printf("[Bluetooth] Mac Address = %02X:%02X:%02X:%02X:%02X:%02X\r\n", btmac[0], btmac[1], btmac[2], btmac[3], btmac[4], btmac[5]);
+}
+
+void loop() {
+}
+```
+*Serial Output* `[Bluetooth] Mac Address = xx:xx:xx:xx:xx:xx`
+
+
+2.Make the PS3 controller remember the MAC address
+ 1. Download this software [Sixaxis Pair Tool](https://sixaxispairtool.software.informer.com/download/)
+ 2. Connect PS3 Controller with PC.
+ 3. Run 'Sixaxis Pair Tool' and input the MAC address.
+
+**Quote source :** http://gijin77.blog.jp/archives/21966552.html
+
